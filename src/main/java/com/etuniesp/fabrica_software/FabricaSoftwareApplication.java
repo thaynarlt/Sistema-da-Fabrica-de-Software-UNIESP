e038c4b63fb90d07265b718fec87e0882f513581
Thayna <tatarlt7@gmail.com>
2025-10-02 15:39:17 -0300

    first commit

---
+++ src/main/java/com/etuniesp/fabrica_software/FabricaSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.etuniesp.fabrica_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etuniesp/fabrica_software/FabricaSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etuniesp.fabrica_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricaSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
